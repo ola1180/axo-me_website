3685232a9cfc15aadef363993c70353575827e0c
Aleksandra Kurdel <aleksandra.kurdel@gmail.com>
2023-05-06 14:46:32 +0200

    added animation slide to navigation

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Navigation
+
+document.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+
+    if (window.scrollY > 0) {
+        nav.classList.add('color-scroll');
+    } else {
+        nav.classList.remove('color-scroll');
+    }
+    });
